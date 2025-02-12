388a17817c62b25ca4a28755d2eb2ca793b3fdd7
alef <alef@kamote>
2025-02-12 09:06:37 +0800

    286a4820-fc7d-40ef-8a43-72e35bc2a87d

--- config.js
+++ config.js
@@ -343,67 +343,29 @@ var klaroConfig = {
             required: true,
         },
         {
-            name: 'google-tag-manager',
-            required: true,
-            purposes: ['marketing'],
-            onAccept: `
-                // we notify the tag manager about all services that were accepted. You can define
-                // a custom event in GTM to load the service if consent was given.
-                for(let k of Object.keys(opts.consents)){
-                    if (opts.consents[k]){
-                        let eventName = 'klaro-'+k+'-accepted'
-                        dataLayer.push({'event': eventName})
-                    }
+        name: 'googleAnalytics', // Choose a descriptive name
+        title: 'Google Analytics', // User-friendly title
+        description: 'Used to analyze website traffic and user behavior.', // Explain why you use it
+        // You're using gtag.js directly, so you don't need the 'default' or 'html' properties here.
+        // Instead, use only the 'callback' property
+        callback: function(consent) {
+          if (consent) {
+            // User has given consent.  gtag.js is already loaded, so just ensure it's active.
+            // No need to add the script again. Just make sure the config is in place.
+          } else {
+            // User has withdrawn consent.  You should disable Google Analytics.
+            // For gtag.js, this means removing the 'config' command.  You can do this by setting a global variable and checking it in your gtag calls.
+            window.gaConsent = false; // Set a flag
+            // Modify your gtag function:
+            window.gtag = function() {
+                if (window.gaConsent) {
+                    dataLayer.push(arguments);
+                } else {
+                    // Do nothing or log a message
+                    console.log("Google Analytics disabled due to user consent.");
                 }
-            `,
-            onInit: `
-                // initialization code here (will be executed only once per page-load)
-                window.dataLayer = window.dataLayer || [];
-                window.gtag = function(){dataLayer.push(arguments)}
-                gtag('consent', 'default', {'ad_storage': 'denied', 'analytics_storage': 'denied', 'ad_user_data': 'denied', 'ad_personalization': 'denied'})
-                gtag('set', 'ads_data_redaction', true)
-            `,
-        },
-        {
-            // In GTM, you should define a custom event trigger named `klaro-google-analytics-accepted` which should trigger the Google Analytics integration.
-            name: 'google-analytics',
-            cookies: [
-                /^_ga(_.*)?/ // we delete the Google Analytics cookies if the user declines its use
-            ],
-            purposes: ['marketing'],
-            onAccept: 
-                // we grant analytics storage
-                gtag('consent', 'update', {
-                    'analytics_storage': 'granted',
-                })
-            ,
-            onDecline: 
-                // we deny analytics storage
-                gtag('consent', 'update', {
-                    'analytics_storage': 'denied',
-                })
-            ,
-        },
-        {
-            name: 'google-ads',
-            cookies: [],
-            onAccept: `
-                // we grant ad storage and personalization
-                gtag('consent', 'update', {
-                    'ad_storage': 'granted',
-                    'ad_user_data': 'granted',
-                    'ad_personalization': 'granted'
-                })
-            `,
-            onDecline: `
-                // we decline ad storage and personalization
-                gtag('consent', 'update', {
-                    'ad_storage': 'denied',
-                    'ad_user_data': 'denied',
-                    'ad_personalization': 'denied'
-                })
-            `,
-            purposes: ['marketing'],
+            };
         }
+      }
     ],
 };
