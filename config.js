9b02c8bf8760a214aaaa703aab12d2c8e8a94cc9
alef <alef@kamote>
2025-02-14 00:12:34 +0800

    bc79d5d4-fe0e-4da8-b7c6-b80f71389c0c

--- config.js
+++ config.js
@@ -241,7 +241,7 @@ var kConfig = {
           _paq.push(['trackPageView']);
           _paq.push(['enableLinkTracking']);
           (function() {
-            var u="//matomo.alipyo.com/";
+            var u="//tracker.alipyo.com/";
             _paq.push(['setTrackerUrl', u+'matomo.php']);
             _paq.push(['setSiteId', '1']);
             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
