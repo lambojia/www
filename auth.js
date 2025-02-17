9bd4a460ae9539b7c2f8c943de3e91f1a6260b90
alef <alef@kamote>
2025-02-17 23:30:06 +0800

    9f84275b-a7f0-4166-bddd-fcb451bbb26f

--- auth.js
+++ auth.js
@@ -110,3 +110,15 @@ function signMeOut() {
 
 }
 
+function initKlaro() {
+
+    klaro.show(kConfig, true);
+    
+    klaro.getManager(kConfig).watch({
+    update: function(obj, name, data) {
+        if (name === 'saveConsents')
+            console.log(data);
+    }
+});
+
+}
